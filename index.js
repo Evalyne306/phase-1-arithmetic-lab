9edc998633593707259586065d09b889ba423417
Evalyne306 <evalyne.wambui@student.moringaschool.com>
2022-11-24 14:41:19 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+const num1= 2
+const num2 =31
+const multiply = num1*num2
+let random= 2
+Math.floor(Math.random() * 10) + 1;
+const num3= 34
+const num4 =5
+const mod =(num3%num4)
+let max = 20
+console.log(Math.max(15,16,17,18,19,20));
+
+
